fe1d864e0ef7223b4d85259d3b349c079c5ac8d4
Miika Savela <miika.a.savela@gmail.com>
2023-04-01 09:42:28 +0300

    Add Tailwind to the project

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<section class="container mt-8">
+		<h1 class="text-2xl">Kuntofutista</h1>
+		<p>Löydä lähin kuntofutis ja aloita jalkapallon pelaaminen hyvässä seurassa!</p>
+	</section>
+</main>
